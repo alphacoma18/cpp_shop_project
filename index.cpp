76e9f7f70fb59010225d794379be1e109a935f2e
fit-Ian-Sioson <115637077+fit-Ian-Sioson@users.noreply.github.com>
2022-11-21 08:25:40 +0800

    Nag add na ako ng switch statemennt for menu

--- index.cpp
+++ index.cpp
@@ -5,9 +5,58 @@ void exitBanner();
 
 int main()
 {
-    shopBanner();
-    exitBanner();
+     shopBanner();
+    int choice, menu, addMore, sumOfOrders;
+    bool quit = false;
+    
+    do{
+    	cout<<"Welcome to KainDerya! Enter the assigned number for our menu."<< endl;
+    	cout<<"1. Menudo - P40"<<endl;
+    	cout<<"2. Afritada - P50"<<endl;
+    	cout<<"3. Adobo - P60"<<endl;
+    	cout<<"4. Sinigang - P40"<<endl;
+    	cout<<"5. Dinakdakan - P60"<<endl;
+    	cout<<"6. Exit"<<endl;
+    	cin>>choice;
+	
+	switch (choice){
+		case 1:
+			cout<<"Excellent!" <<endl;
+			
+			break;
+		case 2:
+		    cout<<"Excellent!"	<<endl;
+		    cin>>choice;
+		    break;
+		case 3:
+			cout<<"Excellent!" <<endl;
+			cin>>choice;
+			break;
+		case 4:
+			cout<<"Excellent!" <<endl;
+			cin>>choice;
+			break;
+		case 5:
+			cout<<"Excellent!" <<endl;
+			cin>>choice;
+			break;
+		case 6:
+				exitBanner();
+				quit = true;
+			break;
+		default:
+			cout<<"Bad choice! Please try again!"<<endl;
+			
+			
+		}
+		if // try adding a feature where they cann add more from their order coma
+				
+		}
+	}
+    
+    while(quit == false);{
     return 0;
+	}
 }
 void shopBanner()
 {
